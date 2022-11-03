190dfbc699fb7c736dc252e023c1219569327dc3
sagarsanjib <sanjibchandan123@gmail.com>
2022-11-03 13:29:30 +0530

    props2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import CardList from './Components/CardList';
+
 
 function App() {
+  const itemData = [
+    {
+      name: "Bright",
+      poster:"https://wallpapercave.com/wp/wp2354003.jpg",
+      img: 'https://i0.wp.com/www.ourmovielife.com/wp-content/uploads/2017/12/Bright-movie-banner.jpg?fit=1280%2C640&ssl=1',
+      title: 'Collection',
+      about: 'Most Popular around the World',
+      featured: true,
+      year:2017,
+      actor_name:"David Ayer",
+      time:"117 min",
+      text:"A detective must work with an Orc to find a powerful wand before evil creatures do."
+
+    },
+    {
+      name: "Tomb Raider",
+      poster:"https://m.media-amazon.com/images/M/MV5BOTY4NDcyZGQtYmVlNy00ODgwLTljYTMtYzQ2OTE3NDhjODMwXkEyXkFqcGdeQXVyNzYzODM3Mzg@._V1_.jpg",
+      img: 'https://c4.wallpaperflare.com/wallpaper/532/570/514/movie-tomb-raider-2018-alicia-vikander-lara-croft-wallpaper-preview.jpg',
+      title: 'Collection',
+      about: 'Great for team building',
+      year:2017,
+      actor_name:"Alicia Vikander",
+      time:"117 min",
+      text:"Lara Croft (Alicia Vikander), must push herself beyond her limits when she discovers the island where her father, Lord Richard Croft disappeared."
+
+    },
+    {
+      name: "Avengers",
+      poster:"http://static.hd-trailers.net/images/AVG-Payoff-1-Sht-v13-jpg_050023.jpg",
+      img: 'https://wallpapercave.com/wp/wp2547005.jpg',
+      title: 'Collection',
+      about: 'Fun for the family',
+      year:2017,
+      actor_name:"Robert Downey Jr",
+      time:"117 min",
+      text: "Earth's mightiest heroes must come together and learn to fight as a team the mischievous Loki and his alien army from enslaving humanity."
+
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardList data={itemData} />
+
     </div>
   );
 }
